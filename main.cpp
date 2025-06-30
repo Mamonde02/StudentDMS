e14bf2f45d5a1d0a732698a2c0f305a64b61e9b1
Mamonde02 <valerosojohnraymund@gmail.com>
2025-06-30 14:02:50 +0800

    add statement console

--- main.cpp
+++ main.cpp
@@ -114,6 +114,29 @@ bool login()
 {
     string username;
     int attempts = 3;
+
+    cout << "=============================\n";
+    cout << BLUE << "  Admin Login System\n";
+    cout << RESET;
+    cout << "=============================\n";
+    cout << "New admin? press 1 to Register here.\n";
+    cout << "1. Register New Admin\n";
+    cout << "2. Login with Existing Admin\n";
+    cout << "Enter your choice (1 or 2): ";
+    int choice;
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        registerAdmin();
+        return true;
+    }
+    else if (choice != 2)
+    {
+        cout << "Invalid choice. Exiting...\n";
+        return false;
+    }
+
     while (attempts > 0)
     {
         cout << "Enter username: ";
@@ -143,55 +166,7 @@ bool login()
         }
     }
 
-    // cout << "=============================\n";
-    // cout << BLUE << "  Admin Login System\n";
-    // cout << RESET;
-    // cout << "=============================\n";
-    // cout << "New admin? press 1 to Register here.\n";
-    // cout << "1. Register New Admin\n";
-    // cout << "2. Login with Existing Admin\n";
-    // cout << "Enter your choice (1 or 2): ";
-    // int choice;
-    // cin >> choice;
-
-    // if (choice == 1)
-    // {
-    //     registerAdmin();
-    //     return login();
-    // }
-    // else if (choice != 2)
-    // {
-    //     cout << RED << "Invalid choice. Please try again.\n"
-    //          << RESET;
-    //     return login();
-    // }
-
-    // cout << "Username: ";
-    // cin >> username;
-
-    // cout << "Password: ";
-    // string password = getMaskedPassword();
-    // // cin >> password;
-
-    // if (verifyCredentials(username, password))
-    // {
-    //     cout << GREEN << "\n Login successful!\n";
-    //     return true;
-    // }
-    // else
-    // {
-    //     // cout << RED << "\n Invalid credentials. Please try again.\n"
-    //     //      << RESET;
-    //     // // return false;
-    //     // return login();
-    //     // using while loop
-    //     while (true)
-    //     {
-    //         cout << RED << "\n Invalid credentials. Please try again.\n"
-    //              << RESET;
-    //         return login();
-    //     }
-    // }
+    return false;
 }
 
 // error handling for invalid input
