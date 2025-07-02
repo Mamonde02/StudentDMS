6a78c8bb7cae6f7ac190eaeed6a7ca06e185cd04
Mamonde02 <valerosojohnraymund@gmail.com>
2025-07-02 23:32:52 +0800

    add initial separate header file student

---
+++ student.h
@@ -0,0 +1,19 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+#include <vector>
+
+struct Studentfrom
+{
+    int id;
+    std::string name;
+    int age;
+};
+
+// Function declarations
+Studentfrom parseStudentfromstudent(const std::string &line);
+std::vector<Studentfrom> readAllStudentsfromstudent();
+void displayStudents();
+
+#endif
